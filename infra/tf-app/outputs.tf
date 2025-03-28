7f81ccd3d94b31c705c1b0043199ffcfe6deca70
B-Crolly <crol0005@algonquinlive.com>
2025-03-28 16:10:15 -0400

    fixed infra-ci-cd formatting problems, also added infra-static tests

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -1,5 +1,5 @@
 output "static_web_app_url" {
-  value       = azurerm_static_site.web.default_host_name
+  value       = azurerm_static_web_app.web.default_host_name
   description = "The URL of the static web app"
 }
 
